bc67ce9398b0774a426f55ffeaf7c751770b9508
lprashant73 <prashant.leichombam@gmail.com>
2023-11-16 01:39:44 +0530

    asynchronous-progs & promise

--- trail.js
+++ trail.js
@@ -1,20 +1,21 @@
-/* console.log("Start");
-function b() {
+//Asynchronous programming in Javascript.
+/* function b() {
     let x = 0;
     for (i = 0; i <= 1000000000; i++) {
         x++;
     }
     console.log('The loop has been completed.')
-};
-function a() {
+}; */
+/* function a() {
     setTimeout(() => {
         console.log("Hello there, how are you?");
     }, 5000);
-}; 
+};
 a();
-b();
+//b();
+console.log(x)
 console.log("Finish"); */
-function b(p, cb) {
+/* function b(p, cb) {
     let r = p + 5;
     let a = cb(r);
     return a;
@@ -33,4 +34,28 @@ function a(x, cb) {
     console.log('The value of z is :', z);
     console.log('Finish');
 };
-a(5, b);
+a(5, b); */
+console.log('Start')
+async function a() {
+    let result;
+    try {
+        result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const success = true;
+                if (success) {
+                    resolve('Resolved return this value.');
+                }
+                else {
+                    reject(new Error('failed to fetch.'));
+                }
+            }, 5000)
+        });
+        console.log('Data fetched by the promise: ', result)
+
+    } catch {
+        console.log(err)
+    };
+    ;
+};
+a();
+console.log('finished')
